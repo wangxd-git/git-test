b180d09470731eff7b31a7a67574134048e33004
wangxd <wangxd@163.com>
2021-01-03 14:17:54 +0800

    把本地工程添加到git

---
+++ git-test/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello git-test");
+        System.out.println("hello git-test");
+    }
+}
